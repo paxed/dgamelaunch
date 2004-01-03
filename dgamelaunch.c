ede1d2cd91e8f21f85b6c93f1b7802d3bc0a2d84
Brett Carrington <brettcar@segvio.org>
2004-01-03 21:57:22 +0000

    See comments in file (use diff if you can't find them)
    git-svn-id: svn://katsu.triplehelix.org/dgamelaunch/trunk@56 db0b04b0-f4d1-0310-9a6d-de3e77497b0e

--- dgamelaunch.c
+++ dgamelaunch.c
@@ -58,7 +58,8 @@
 
 #ifdef __linux__
 # include <pty.h>
-# include <libiberty.h>
+/*# include <libiberty.h> -- NOT IN MINIAL-TYPE INSTALLATIONS 
+ * Also, wasn't needed for compiling at current revision - brettcar */
 #endif
 
 #ifndef ARRAY_SIZE
