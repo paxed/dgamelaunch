195c938c53f3d975da609695886d81e675a0a2fb
Jilles Tjoelker <jilles@stack.nl>
2004-02-15 23:10:33 +0000

    Simple stress-test script.
    git-svn-id: svn://katsu.triplehelix.org/dgamelaunch/trunk@235 db0b04b0-f4d1-0310-9a6d-de3e77497b0e

---
+++ stresstest.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# stress-test script
+# requires users test1...test${max} to be defined in /dgl-login, /dgldir/ttyrec
+# and /dgldir/rcfiles.
+# current directory must contain the dgamelaunch to be used
+
+# Be sure to change these variables
+conffile="test2.conf"
+chrootdir="chroot-2"
+hackdir="/usr/local/lib/nethack"
+max=19
+
+# clean up previous cruft
+rm -f "$chrootdir"/dgldir/inprogress/*
+rm -f "$chrootdir$hackdir"/save/*[0-9]test[0-9]*
+
+export conffile i
+i=1
+while [ $i -le $max ]; do
+	xterm -e sh -c '(sleep 1; echo "ltest$i"; echo aa; echo -n py i; cat) | ./dgamelaunch -f "$conffile" '&
+	sleep 1
+	i=$(($i+1))
+done
+
