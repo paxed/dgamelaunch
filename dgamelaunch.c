d54512a31f1cf51b2b80f371270e35b1974ee8a8
Pasi Kallinen <paxed@alt.org>
2013-03-04 17:26:06 +0200

    Add a -c option (login shell command) with an error message. (from Edoardo Spadolini)

--- dgamelaunch.c
+++ dgamelaunch.c
@@ -2719,10 +2719,15 @@ main (int argc, char** argv)
 
   __progname = basename(strdup(argv[0]));
 
-  while ((c = getopt(argc, argv, "sqh:pi:aeW:SD")) != -1)
+  while ((c = getopt(argc, argv, "c:sqh:pi:aeW:SD")) != -1)
   {
     switch (c)
     {
+      case 'c':
+        fprintf(stderr, "This is not a shell account, you can't execute "
+                        "arbitrary commands.\n");
+        graceful_exit(15);
+        break;
       case 's':
 	showplayers = 1; break;
 
